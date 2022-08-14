3dadc39b42400f71adc013b6e06ff1630c42e096
XiangYuXiHu <1278043194@qq.com>
2022-08-14 10:14:41 +0800

    sink redis

---
+++ src/main/java/com/smile/sink/MyRedisSink.java
@@ -0,0 +1,46 @@
+package com.smile.sink;
+
+import com.smile.domain.Event;
+import com.smile.source.MyRedisSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.redis.RedisSink;
+import org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisPoolConfig;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommand;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommandDescription;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisMapper;
+
+/**
+ * @Description
+ * @ClassName RedisSink
+ * @Author smile
+ * @date 2022.08.14 09:58
+ */
+public class MyRedisSink {
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        FlinkJedisPoolConfig jedisPoolConfig = new FlinkJedisPoolConfig.Builder().setHost("localhost").build();
+        environment.addSource(new MyRedisSource()).addSink(new RedisSink<>(jedisPoolConfig, new MyRedisMapper()));
+
+        environment.execute("redis sink");
+    }
+
+    public static class MyRedisMapper implements RedisMapper<Event> {
+
+        @Override
+        public RedisCommandDescription getCommandDescription() {
+            return new RedisCommandDescription(RedisCommand.HSET, "clicks");
+        }
+
+        @Override
+        public String getKeyFromData(Event event) {
+            return event.getUser();
+        }
+
+        @Override
+        public String getValueFromData(Event event) {
+            return event.getUrl();
+        }
+    }
+}
---
+++ src/main/java/com/smile/source/MyRedisSource.java
@@ -0,0 +1,36 @@
+package com.smile.source;
+
+import com.smile.domain.Event;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Description
+ * @ClassName MyRedisSource
+ * @Author smile
+ * @date 2022.08.14 10:01
+ */
+public class MyRedisSource implements SourceFunction<Event> {
+
+    private Boolean isRunning = true;
+
+    @Override
+    public void run(SourceContext<Event> ctx) throws Exception {
+        Random random = new Random();
+
+        String[] users = new String[]{"mary", "slice", "lucy", "smile"};
+        String[] urls = new String[]{"./hello", "./game", "./hai", "./name"};
+        while (isRunning) {
+            ctx.collect(new Event(users[random.nextInt(users.length)], urls[random.nextInt(urls.length)], System.currentTimeMillis()));
+
+            TimeUnit.SECONDS.sleep(3);
+        }
+    }
+
+    @Override
+    public void cancel() {
+        isRunning = false;
+    }
+}
