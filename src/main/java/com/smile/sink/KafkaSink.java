6f803696744956b745cacdd21bca60fc57b56e7e
XiangYuXiHu <1278043194@qq.com>
2023-04-05 08:14:34 +0800

    kafka sink

--- src/main/java/com/smile/sink/KafkaSink.java
+++ src/main/java/com/smile/sink/KafkaSink.java
@@ -4,16 +4,13 @@ import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
-import org.apache.flink.streaming.connectors.kafka.KafkaSerializationSchema;
+import org.apache.flink.streaming.connectors.kafka.*;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
 import javax.annotation.Nullable;
 import java.util.Properties;
 
 /**
- *
  * @Description
  * @ClassName KafkaSink
  * @Author smile
@@ -24,12 +21,17 @@ public class KafkaSink {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
 
+        kafkaSink(environment);
+    }
+
+    public static void kafkaSink(StreamExecutionEnvironment environment) throws Exception {
         Properties properties = new Properties();
         properties.setProperty("bootstrap.servers", "192.168.21.120:9092");
         DataStreamSource<String> streamSource = environment.addSource(
                 new FlinkKafkaConsumer<String>("fk-stream-in-topic", new SimpleStringSchema(), properties));
 
         FlinkKafkaProducer flinkKafkaProducer = new FlinkKafkaProducer("fk-stream-out-topic", new KafkaSerializationSchema<String>() {
+
             @Override
             public ProducerRecord<byte[], byte[]> serialize(String element, @Nullable Long timestamp) {
                 return new ProducerRecord<>("fk-stream-out-topic", element.getBytes());
